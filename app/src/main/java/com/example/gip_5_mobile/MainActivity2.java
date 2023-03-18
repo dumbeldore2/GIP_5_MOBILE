c460036215218ff4d01b1939e9ef4caaa724e188
PC001 laptop <yago.engels@gmail.com>
2023-03-18 14:32:47 +0100

    MainActivity2 deels opgestart

--- app/src/main/java/com/example/gip_5_mobile/MainActivity2.java
+++ app/src/main/java/com/example/gip_5_mobile/MainActivity2.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    //initen van views
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //conecten van views
+        //functies
     }
 }
