2648b24db3e8844cbaa139e2987c1896af55a970
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-03-20 21:32:01 +0100

    eerste api call is al gelukt

--- app/src/main/java/com/example/gip_5_mobile/MainActivity.java
+++ app/src/main/java/com/example/gip_5_mobile/MainActivity.java
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    // url string
+    String url = "http://192.168.1.103:8080/api/v1/";
 
     //initen van views
     EditText editText1,editText2;
@@ -18,20 +30,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //views conecten
         editText1 = findViewById(R.id.email);
         editText2 = findViewById(R.id.password);
         button1 = findViewById(R.id.login);
+
 
         //functie
         click_fun_1();
     }
 
     public void click_fun_1(){
-        button1.setOnClickListener(V -> {
-            // check password hier
-            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-            startActivity(intent);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                System.out.println("ello");
+                //api request
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url + "/user/getall", null,
+                        new Response.Listener<JSONObject>() {
+                            @Override public void onResponse(JSONObject response) {
+                                System.out.println(response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override public void onErrorResponse(VolleyError error) {
+                        System.out.println(error);
+                    }
+                });
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(jsonObjectRequest);
+
+                //Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                //startActivity(intent);
+            }
         });
     }
 
