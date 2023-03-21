8cc4a4f558ee2c6db1f4df073ad532a1aeb72d3e
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-03-21 18:31:52 +0100

    api headers geprobeert

--- app/src/main/java/com/example/gip_5_mobile/MainActivity.java
+++ app/src/main/java/com/example/gip_5_mobile/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -15,6 +17,11 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,19 +51,44 @@ public class MainActivity extends AppCompatActivity {
     public void click_fun_1(){
         button1.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
-                System.out.println("ello");
                 //api request
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url + "/user/getall", null,
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                        url + "user/email" +
+                        "/yago.engels@gmail.com", null,
                         new Response.Listener<JSONObject>() {
                             @Override public void onResponse(JSONObject response) {
                                 System.out.println(response.toString());
                             }
                         }, new Response.ErrorListener() {
                     @Override public void onErrorResponse(VolleyError error) {
-                        System.out.println(error);
+                        System.out.println(error.toString());
                     }
-                });
-                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                }){
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError{
+                        Map<String, String> headers = new HashMap<>();
+
+                        headers.put("Content-Type", "application/json;charset=UTF-8");
+
+                        //autherization
+                        String auth = getEditText1() + ":" + getEditText2();
+                        byte [] bytes = auth.getBytes(StandardCharsets.UTF_8);
+                        String base = Base64.getEncoder().encodeToString(bytes);
+                        System.out.println(base);
+
+
+
+                        headers.put("Authorization", "Basic " + auth);
+                        //headers.put("Content-Length", "<calculated when request is sent>");
+                        //headers.put("Host", "<calculated when request is sent>");
+                        //headers.put("Accept-Encoding", "gzip, deflate,br");
+                        //headers.put("Connection", "keep-alive");
+                        return headers;
+                    }
+                };
+
                 requestQueue.add(jsonObjectRequest);
 
                 //Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
